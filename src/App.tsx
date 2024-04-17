0977560032188a4187a94a1fcaf48fa4da15c622
Leenard <lolleenard0@gmail.com>
2024-04-17 21:03:39 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <main className="w-screen h-screen flex flex-col items-center justify-center">
+      <h1 className="text-8xl font-geist font-bold tracking-tight">
+        🔥UI Scaffold🔥
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Made with latest and production grade technologies.
+      </p>
+    </main>
+  );
+}
+
+export default App;
