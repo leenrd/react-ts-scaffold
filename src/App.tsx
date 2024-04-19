239fb358de74e9c1a58611707975d376011fa765
Leenard <lolleenard0@gmail.com>
2024-04-19 17:12:16 +0800

    style: init

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ModeToggle } from "./components/theme-toggle";
 function App() {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-background bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
       <h1 className="text-8xl font-geist font-bold tracking-tight">
         🔥UI Scaffold🔥
       </h1>
